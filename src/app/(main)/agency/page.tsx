2f1299e5e10d63fc8e6b0c665ab952df851a8f8c
Joan Romero <romerojoan1999@gmail.com>
2024-06-16 11:45:12 -0400

    feat: :sparkles: add prisma setup

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+	return (
+		<div className="flex justify-center items-center mt-4">
+			<div className="max-w-[850px] border-[1px] p-4 rounded-xl">
+				<h1 className="text-4xl"> Create An Agency</h1>
+			</div>
+		</div>
+	);
+}
